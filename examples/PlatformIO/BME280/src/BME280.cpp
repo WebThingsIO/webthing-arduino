493d6696f98246f533baab6ee26c1d88cd877d95
Michael Stegeman <michael@stegeman.me>
2019-12-30 09:25:43 -0900

    Fix CI.

--- examples/PlatformIO/BME280/src/BME280.cpp
+++ examples/PlatformIO/BME280/src/BME280.cpp
@@ -59,7 +59,7 @@ BME280I2C::Settings settings(
    BME280::StandbyTime_1000ms,
    BME280::Filter_Off,
    BME280::SpiEnable_False,
-   0x76 // I2C address. I2C specific.
+   (BME280I2C::I2CAddr) 0x76 // I2C address. I2C specific.
 );
 
 BME280I2C bme(settings);
